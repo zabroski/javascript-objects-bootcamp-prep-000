451b28abb0a19c5687173dd44f6a10567a3a2322
Issouf zabre <sheridanzabre@gmail.com>
2018-06-05 03:56:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist,artist,song){
   return playlist
 }
 
-function updatePlaylist(playlist,artist,song){
-  playlist[artist]=song
+function removeFromPlaylist(playlist,artist){
+  delete playlist[artist]
   return playlist
 }
